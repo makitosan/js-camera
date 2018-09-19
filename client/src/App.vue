83a2befc0392758e54e32e5179356cf740316cdc
Aki <makitosansan@gmail.com>
2018-09-19 19:27:36 +0900

    Add camera and capture client feature

--- client/src/App.vue
+++ client/src/App.vue
@@ -7,51 +7,72 @@
         <div>
           <button id="capture" @click="captureShot">CAPTURE</button>
         </div>
+        <div>
+          <button id="uploadButton" @click="upload">UPLOAD</button>
+        </div>
         <canvas id="picture"></canvas>
         <p>{{msg}}</p>
     </div>
 </template>
 
 <script>
-    var constraints = {
-        video: {
-        },
-    }
-    export default {
-        name: 'app',
-        created: function() {
-          console.log('created');
-          //デバイス情報ゲット
-          var item = navigator.mediaDevices.getUserMedia(constraints);
-          //ストリーム情報をビデオタグのsrcに設置
-          item.then( function(stream) {
-             var video = document.querySelector('video');
-             video.src = window.URL.createObjectURL(stream);
-             video.onloadedmetadata = function(e) {
-                  //ロード完了後にカメラの設定
-             };
-          });
+import axios from 'axios';
 
-        },
-        data: function() {
-            return {
-                msg: 'this is a message.'
-            }
-        },
-        methods: {
-          captureShot: function() {
-            var videocnt = document.getElementById('video');
-            var canvas = document.getElementById('picture');
-            console.log(videocnt.width);
-            canvas.width = 640;
-            canvas.height = 480;
-            var cnt2d = canvas.getContext('2d');
-            //canvasに書き込み
-            cnt2d.drawImage(videocnt, 0, 0);
-            this.msg = 'new image captured..' + (new Date());
-          }
+var constraints = {
+    video: {
+    },
+}
+export default {
+    name: 'app',
+    created: function() {
+      console.log(`${process.env.API_ROOT}`);
+      this.apiEndpoint = `${process.env.API_ROOT}`;
+      console.log('created');
+      //デバイス情報ゲット
+      var item = navigator.mediaDevices.getUserMedia(constraints);
+      //ストリーム情報をビデオタグのsrcに設置
+      item.then( function(stream) {
+         var video = document.querySelector('video');
+         if('srcObject' in video) {
+           video.srcObject = stream;
+         } else {
+           video.src = window.URL.createObjectURL(stream);
+         }
+         video.onloadedmetadata = function(e) {
+              //ロード完了後にカメラの設定
+         };
+      });
+
+    },
+    data: function() {
+        return {
+            msg: 'this is a message.',
+            apiEndpoint: null
         }
+    },
+    methods: {
+      captureShot: function() {
+        var videocnt = document.getElementById('video');
+        var canvas = document.getElementById('picture');
+        console.log(videocnt.width);
+        canvas.width = 640;
+        canvas.height = 480;
+        var cnt2d = canvas.getContext('2d');
+        //canvasに書き込み
+        cnt2d.drawImage(videocnt, 0, 0);
+        this.msg = 'new image captured..' + (new Date());
+        let base64 = canvas.toDataURL('image/png');
+        console.log(base64);
+      },
+      upload: function() {
+        let canvas = document.getElementById('picture');
+        let base64 = canvas.toDataURL('image/png');
+        axios.post(this.apiEndpoint, {
+          img: base64
+        });
+      }
     }
+}
 </script>
 
 <style lang="css">
